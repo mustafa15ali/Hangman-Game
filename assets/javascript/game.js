019f630c61de88e4e86286a4e2fa17d9525ccf0a
Mustafa Farah <mustafa.farah@outlook.com>
2018-03-02 00:23:40 -0600

    this shit hard

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,20 @@
-(function () {
-        "use strict";
-        var wins, losses, maxErrors, validGuesses, pressAnyKeyToStart, pressAnyKeyToRestart, youWin, youLose, emptyAlert,
-            function setup() {
-                /* start config options */
-                win = 0;
-                losses = 0;
-                maxErrors = 9;
-                validGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-                pressAnyKeyToStart =
-                pressAnyKeyToRestart =
-                youWin =
-                youLose =
-                emptyAler =
-                    /* end of config options */
 
-        
+function hangMenGame() {
+    var hangMenGame;
+    confirm("Do you want prove your gaming worth?")
+        if (true[prompt("What's your name?")]);
+    }   else { alert("You are a scardy cat!");
+}
 
-            }
+
+function hangMenGame() {
+    var txt;
+    if (confirm("Do you want prove your gaming worth?")) {
+        txt = "You have 9 lives!";
+        Console.log("Yes or no")
+    } else {
+        (alert("You are a scardy cat!")) {
+            console.log("alert thing")
+        }
+
     }
