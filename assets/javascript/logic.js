e1b97065b8109d0005b4821bd4c411bdcf9e705f
Mustafa Farah <mustafa.farah@outlook.com>
2018-03-04 15:08:31 -0600

    changed name of js file and added all global arrays & variables

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,21 @@
+// Global VARIABLES 
+//-----------------------------------------------------------------------
+// Arrays and Variables for holding data
+var wordOptions = ["mustafa", "ali", "farah", "abdisalam", "muna", "abas", "ahmed"];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blankAndSuccesses = []; // j _ _ _ _ _ _ _
+var wrongletters = [];
 
+// Game counters
+var winCounter = 0;
+var lossCounter = 0;
+var guessesLeft = 0;
+
+// FUNCTIONS (Reusable blocks of code that I will call upon when needed)
+//-----------------------------------------------------------------------
+
+
+// MAIN PROCESS
+//-----------------------------------------------------------------------
