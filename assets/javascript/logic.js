5b37c77f807169acb37eadad93f8a3ba3da2cd18
Mustafa Farah <mustafa.farah@outlook.com>
2018-03-04 15:49:40 -0600

    added 4 loop and get element ids

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,7 +3,7 @@
 // Arrays and Variables for holding data
 var wordOptions = ["mustafa", "ali", "farah", "abdisalam", "muna", "abas", "ahmed"];
 var selectedWord = "";
-var lettersinWord = [];
+var lettersInWord = [];
 var numBlanks = 0;
 var blankAndSuccesses = []; // j _ _ _ _ _ _ _
 var wrongletters = [];
@@ -16,6 +16,37 @@ var guessesLeft = 0;
 // FUNCTIONS (Reusable blocks of code that I will call upon when needed)
 //-----------------------------------------------------------------------
 
+function startGame () {
+    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+    lettersInWord = selectedWord.split("");
+    lettersInWord = lettersInWord.length;
+
+    // Reset
+    guessesLeft = 9;
+    wrongletters = [];
+    blankAndSuccesses = [];
+
+    // Populate blanks and successes with right number of blanks.
+    for (var i=0; i<numBlanks; i++) {
+        blankAndSuccesses[i].push("_");
+    }
+
+    // Change HTML to reflect round conditions
+    document.getElementById("wordToGuess").innerHTML = blankAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCounter;
+    document.getElementById("lossCounter").innerHTML = lossCounter;
+
+    // Testing/ Debugging
+    console.log(selectedWord);
+    console.log(lettersInWord);
+    console.log(numBlanks);
+    console.log(blankAndSuccesses);
+}
+
+
 
 // MAIN PROCESS
 //-----------------------------------------------------------------------
+
+startGame();
